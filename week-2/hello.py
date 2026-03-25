2d323e81bd5f228f5421e4e4c8ede5318dd84d6f
Shanaya Morton <shanayamorton@outlook.com>
2026-03-25 09:35:10 +0000

    Added musical moments, coding and dsiplaying output task

---
+++ week-2/hello.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+
+song = input("What is your favourite song? ")
+print("Your favourite song is:", song)
+
+title = "Blinding Lights"
+artist = "The Weeknd"
+
+print(f"My favourite song is called '{title}', by the artist {artist}.")
